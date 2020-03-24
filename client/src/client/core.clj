79d8042e0c4c62db15e0203aec45a029c0001f93
c20rtn <jack.corton@googlemail.com>
2020-03-24 15:41:43 +0000

    slurps to map structure of maze

--- client/src/client/core.clj
+++ client/src/client/core.clj
@@ -2,4 +2,6 @@
   (:require [clojure.data.json :as json]))
 
 (defn get-maze [x]
-  (json/read-str(slurp (str "http://localhost:3000/maze/" x))))
+  (json/read-str
+    (slurp (str "http://localhost:3000/maze/" x))
+    :key-fn keyword))
